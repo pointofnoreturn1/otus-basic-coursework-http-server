ef56155289b47b594bc3c77b10f60651c25f1ee9
Vadim Kurnyshov <vaku878@gmail.com>
2024-11-17 16:09:39 +0400

    add port specification

--- src/main/java/io/vaku/app/Application.java
+++ src/main/java/io/vaku/app/Application.java
@@ -1,9 +1,24 @@
 package io.vaku.app;
 
 import io.vaku.app.web.HttpServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Application {
+    private static final int DEFAULT_PORT = 8189;
+    private static final Logger logger = LogManager.getLogger(HttpServer.class);
+
     public static void main(String[] args) {
-        new HttpServer(8189).start();
+        if (args.length == 0) {
+            new HttpServer(DEFAULT_PORT).start();
+        } else if (args.length == 2 && args[0].equals("-p")) {
+            try {
+                new HttpServer(Integer.parseInt(args[1])).start();
+            } catch (NumberFormatException e) {
+                logger.error("Failed to parse port: {}", args[1]);
+            }
+        }
+
+        logger.error("Specify port with '-p' argument. Default port is {}", DEFAULT_PORT);
     }
 }
